d0ef073c5da5b8a7e6271b6763d9ff2750bcefd0
joilson <joilsonhungrianv@gmail.com>
2023-08-08 12:15:11 -0300

    feat: implement cors

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,11 @@ import "express-async-errors";
 
 import { routes } from "./routers";
 import { handleError } from "./errors";
+import cors from "cors";
 
 const app = express();
 app.use(json());
+app.use(cors);
 
 app.use("/users", routes.user);
 app.use("/advertisement", routes.advertisement);
