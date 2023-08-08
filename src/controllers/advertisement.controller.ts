4233bbee9d4d2fc3e933fcc353f0d64e8994a26c
joilson <joilsonhungrianv@gmail.com>
2023-08-08 11:50:30 -0300

    fix: verify token middlewars

--- src/controllers/advertisement.controller.ts
+++ src/controllers/advertisement.controller.ts
@@ -3,31 +3,57 @@ import { Request, Response } from "express";
 import advertisementSchema from "../schemas/advertisementSchema";
 import { advertisementServices } from "../services/advertisement.services";
 
-
-const advertisementCreate = async (req: Request, res: Response): Promise<Response> => {
+const advertisementCreate = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
   const reqIsValid = advertisementSchema.advertisementRequest.parse(req.body);
 
-  const advertisement = await advertisementServices.advertisementCreateService(reqIsValid, res.locals.userId);
+  const advertisement = await advertisementServices.advertisementCreateService(
+    reqIsValid,
+    res.locals.userId
+  );
   return res.status(200).json(advertisement);
 };
 
-const advertisementReadAll = async (req: Request, res: Response): Promise<Response> => {
-  const ads = await advertisementServices.advertisementGetAllService()
+const advertisementReadAll = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const teste = req.query.params;
+  console.log(teste);
+
+  const ads = await advertisementServices.advertisementGetAllService();
   return res.status(200).json(ads);
 };
 
-const advertisementRead = async (req: Request, res: Response): Promise<Response> => {
-  const advertisement = await advertisementServices.advertisementGetService(Number(req.params.adsId));
+const advertisementRead = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const advertisement = await advertisementServices.advertisementGetService(
+    Number(req.params.adsId)
+  );
   return res.status(200).json(advertisement);
 };
 
-const advertisementUpdate = async (req: Request, res: Response): Promise<Response> => {
-  const advertisement = await advertisementServices.advertisementUpdateService(req.body, Number(req.params.id),Number(res.locals.userId));
+const advertisementUpdate = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const advertisement = await advertisementServices.advertisementUpdateService(
+    req.body,
+    Number(req.params.id),
+    Number(res.locals.userId)
+  );
   return res.status(200).json(advertisement);
 };
 
-const advertisementDelete = async (req: Request, res: Response): Promise<Response> => {
-  await advertisementServices.advertisementDeleteService(Number(req.params.id))
+const advertisementDelete = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  await advertisementServices.advertisementDeleteService(Number(req.params.id));
   return res.status(200).send();
 };
 
